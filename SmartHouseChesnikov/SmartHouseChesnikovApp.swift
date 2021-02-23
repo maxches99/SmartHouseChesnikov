63e83df7cd9d3d46ee09f7e195494b1e7fe7aacb
Чесников <maxim.chesnikov@chulakov.ru>
2021-02-23 23:45:20 +0300

    Initial Commit

---
+++ SmartHouseChesnikov/SmartHouseChesnikovApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHouseChesnikovApp.swift
+//  SmartHouseChesnikov
+//
+//  Created by Max Chesnikov on 23.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SmartHouseChesnikovApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
